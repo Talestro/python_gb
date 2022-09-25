dd93ff5522a01fd6a364ac60699ec12c3c2eeae7
Talestro <SemenovEM@yandex.ru>
2022-09-25 22:49:56 +0300

    task 3 added

--- homework_1/task_3.py
+++ homework_1/task_3.py
@@ -3,3 +3,20 @@
 # - x=34; y=-30 -> 4
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
+
+x_coordinate: int = int(input('Enter X coordinate: '))
+y_coordinate: int = int(input('Enter Y coordinate: '))
+
+if (x_coordinate == 0 or y_coordinate == 0):
+    if (x_coordinate == 0):
+        print('The point lies on the axis X')
+    else:
+        print('The point lies on the axis Y')
+elif (x_coordinate > 0 and y_coordinate > 0):
+    print('The point in 1 quarter')
+elif (x_coordinate < 0 and y_coordinate < 0):
+    print('The point in 3 quarter')
+elif (x_coordinate > 0 and y_coordinate < 0):
+    print('The point in 4 quarter')
+else:
+    print('The point in 2 quarter')
